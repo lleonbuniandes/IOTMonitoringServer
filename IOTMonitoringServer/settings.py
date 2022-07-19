06beb044f6e50b8a2eff1d53125c7bdd220741ce
Luis Leon <l.leon@uniandes.edu.co>
2022-07-18 21:34:39 -0500

    Cambios de ip

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -161,8 +161,8 @@ MQTT_HOST = "54.197.202.37"
 MQTT_PORT = 8082
 
 # Credenciales para el usuario suscriptor del bróker MQTT
-MQTT_USER = "admin"
-MQTT_PASSWORD = "admin"
+MQTT_USER = "user1"
+MQTT_PASSWORD = "123456"
 
 # Credenciales para el usuario publicador del bróker MQTT
 MQTT_USER_PUB = "admin2"
